be5aca6905f050a78b2a571cecfb83d9a981bff7
Mr.G <nikadoreuli47@proton.me>
2023-09-23 12:52:51 +0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ mount /dev/sda3 /mnt &&
 mount --mkdir /dev/sda1 /mnt/boot/efi &&
 swapon /dev/sda2 &&
 pacman -Syy archlinux-keyring figlet --noconfirm &&
-pacstrap /mnt amd-ucode base base-devel bluez bluez-utils efibootmgr firefox git grub htop intel-ucode linux linux-firmware linux-headers mpv pcmanfm neovim net-tools networkmanager ntfs-3g obs-studio openssh papirus-icon-theme picom pipewire pipewire-alsa pipewire-jack pipewire-media-session pipewire-pulse pulsemixer redshift ttc-iosevka unzip xorg-server xorg-xinit xorg-xsetroot &&
+pacstrap /mnt amd-ucode base base-devel bluez bluez-utils efibootmgr firefox git grub htop intel-ucode linux linux-firmware linux-headers mpv pcmanfm neovim net-tools networkmanager ntfs-3g obs-studio openssh papirus-icon-theme pipewire pipewire-alsa pipewire-jack pipewire-media-session pipewire-pulse pulsemixer redshift unzip xorg-server xorg-xinit xorg-xsetroot &&
 arch-chroot /mnt grub-install /dev/sda &&
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg &&
 arch-chroot /mnt ln -sf /usr/share/zoneinfo/Asia/Tbilisi /etc/localtime &&
