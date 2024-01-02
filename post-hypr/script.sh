61bb005639d847d492aea3ea7f8c11debe4e710b
Mr.G <nikadoreuli47@proton.me>
2024-01-03 00:17:02 +0400

    Update script.sh

--- post-hypr/script.sh
+++ post-hypr/script.sh
@@ -1,6 +1,7 @@
 cd ~ &&
 rm -rf .* &&
 mv ~/full-hypr/post-hypr/.config ~ &&
-# mv ~/full-hypr/post-hypr/***** ~ &&
+mv ~/full-hypr/post-hypr/.bashrc ~ &&
+mv ~/full-hypr/post-hypr/.bash_profile ~ &&
 git clone https://github.com/.wallpapers &&
 cd ~
